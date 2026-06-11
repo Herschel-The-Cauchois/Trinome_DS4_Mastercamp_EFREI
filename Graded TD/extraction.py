dc7f3922619b146faac3d789891fabd9faa9379c
Herschel_TC <raphael.lesterlin@gmail.com>
2026-06-11 12:36:55 +0200

    final in TD commit

--- Graded TD/extraction.py
+++ Graded TD/extraction.py
@@ -5,10 +5,12 @@ Created on Thu Jun 11 09:48:34 2026
 @author: leste
 """
 
+# possible improvement : using time modules to time properly the processes ?
+import re as re
 import feedparser as fp
 import requests as rq
 from time import sleep
-import panda as pd
+import pandas as pd
 url = "https://www.cert.ssi.gouv.fr/feed/"
 rss_feed = fp.parse(url)
 
@@ -37,5 +39,51 @@ for link in feed_extracted["links"]:
     count += 1
 print("CVE listing extracted in {} seconds.".format(count))
 print("Data obtained : ", ref_cves)
+
+cve_pattern = r"CVE-\d{4}-\d{4,7}"
+cve_list = []
+for elem in ref_cves: # Should follow same pattern as above ? only changes what's inside the list
+    cve_list.append(list(set(re.findall(cve_pattern, str(data)))))
+print("Obtained CVEs of length {} : ".format(len(cve_list)), cve_list)
+
+unique_cves = []
+for elem in cve_list:
+    unique_cves += elem
+unique_cves = list(set(unique_cves)) # Conversion to only retrieve unique ids, better for discussing with CVE API
+
+counter = 0
+cve_data = {"id":[], "description":[], "cvss":[], "cwe":[], "cwe_desc":[], "affected":[]} # All info about the CVEs we want to have it handy
+# /!\ affected will contain list of dictionaries with vendor, product name and versions keys, datatype to manage in df creation
+for elem in unique_cves:
+    response = rq.get(f"https://cveawg.mitre.org/api/cve/{elem}")
+    try:
+        data = response.json()
+        cve_data["description"].append(data["containers"]["cna"]["descriptions"][0]["value"])
+        cvss_flag = False # Signals if the value was found in any retrieval attempt
+        try:
+            cve_data["cvss"].append(data["containers"]["cna"]["metrics"][0]["cvssV3_0"]["baseScore"])
+            cvss_flag = True
+        except KeyError:
+            print("CVSS for " + elem + " could not be retrieved at _0, trying at _1")
+        if not cvss_flag:
+            try:
+                cve_data["cvss"].append(data["containers"]["cna"]["metrics"][0]["cvssV3_1"]["baseScore"])
+                cvss_flag = True
+            except KeyError:
+                print("CVSS for " + elem + " could not be retrieved at _1, putting empty value")
+                cve_data["cvss"].append("Unavailable")
+        problemtype = data["containers"]["cna"].get("problemTypes", {})
+        if problemtype and "descriptions" in problemtype[0]:
+            cve_data["cwe"] = problemtype[0]["descriptions"][0].get("cweId", "Unavailable")
+            cve_data["cwe_desc"] = problemtype[0]["descriptions"][0].get("description", "Unavailable")
+            
+        # product to do
+    except rq.JSONDecodeError:
+        print("Adress " + f"https://cveawg.mitre.org/api/cve/{elem}" + " returns malformed JSON or is not available")
+    cve_data["id"].append(elem) # This is done last to prevent phantom ids without any attached data to create an offset in the records
+    sleep(1)
+    count += 1
+print("Final dic obtained in {} seconds: ".format(count), cve_data)
+        
     
 # Objective : produce a dataframe that will be encoded as a csv file for other parts
